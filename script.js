12ae4ea9d523866e3e203a6ccb235c10b37fe6b2
timothyhoover <timmchoover@gmail.com>
2021-01-26 11:08:44 -0600

    improving readability

--- script.js
+++ script.js
@@ -5,14 +5,12 @@ const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
-// Show Loader
-function loading() {
+function showLoadingSpinner() {
   loader.hidden = false;
   quoteContainer.hidden = true;
 }
 
-// Hide Loading
-function complete() {
+function removeLoadingSpinner() {
   if (!loader.hidden) {
     quoteContainer.hidden = false;
     loader.hidden = true;
@@ -21,7 +19,7 @@ function complete() {
 
 // Get Quote From API
 async function getQuote() {
-  loading();
+  showLoadingSpinner();
   const proxyURL = 'https://evening-falls-51219.herokuapp.com/';
   const apiURL =
     'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
@@ -43,7 +41,7 @@ async function getQuote() {
     quoteText.innerText = data.quoteText;
 
     // Stop Loader, Show Quote
-    complete();
+    removeLoadingSpinner();
   } catch (error) {
     getQuote();
   }
